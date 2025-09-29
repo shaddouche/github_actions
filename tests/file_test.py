24574f762a9a28ce7e0d51ce363d7f3140c4088d
Daniel <daniel@datascientest.com>
2025-09-29 12:51:13 +0000

    Control push after correction spaces

--- tests/file_test.py
+++ tests/file_test.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_calc_addition():
 
-    output = 2+4
+    output = 2 + 4
 
     assert output == 6
 
@@ -12,7 +12,7 @@ def test_calc_substraction():
 
     # Fonction test du résultat de 2-4
 
-    output = 2-4
+    output = 2 - 4
 
     assert output == -2
 
@@ -21,7 +21,7 @@ def test_calc_multiply():
 
     # Fonction test du résultat de 2*4
 
-    output = 2*4
+    output = 2 * 4
 
     assert output == 8
 
